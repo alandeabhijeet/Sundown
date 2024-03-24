0db4ab6743ef01669ca15aa1ad087c19d78271a1
Alande Abhijeet <122161608+alandeabhijeet@users.noreply.github.com>
2024-03-24 10:46:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+var loader = document.querySelector("#loader")
+    setTimeout(()=> {
+        loader.style.top = "-100%"
+    }, 4200);
+
+    // const scroll = new LocomotiveScroll({
+    //     el: document.querySelector('#main'),
+    //     smooth: true
+    // });
